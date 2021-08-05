0a64b7207929630dd8aec3d421115045157c97d7
Joesh Gichon <joeshgichon56@gmail.com>
2021-08-05 14:11:58 +0300

    This will style the founders section

--- js/about.js
+++ js/about.js
@@ -20,3 +20,35 @@ $(document).ready(function(){
       }
     });
 });
+
+
+$(document).ready(function(){
+    $("#b1").click(function(){
+        $("#p1").toggle();
+    });
+});
+$(document).ready(function(){
+    $("#b2").click(function(){
+        $("#p2").toggle();
+    });
+});
+$(document).ready(function(){
+    $("#b3").click(function(){
+        $("#p3").toggle();
+    });
+});
+$(document).ready(function(){
+    $("#b4").click(function(){
+        $("#p4").toggle();
+    });
+});
+$(document).ready(function(){
+    $("#b5").click(function(){
+        $("#p5").toggle();
+    });
+});
+$(document).ready(function(){
+    $("#b6").click(function(){
+        $("#p6").toggle();
+    });
+});
